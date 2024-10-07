62027db694b872bbcd6fbc6bc8aa87f46907a082
Bravestone01 <mujtabashah906@gmail.com>
2024-10-07 17:04:07 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
+import React,{useEffect , useState} from "react";
+
+
+
 
 export default function App() {
+
+  const [data ,setData]=useState([])
+  const dataApi = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    let result = await fetch(url);
+    result =await result.json();
+    setData(result)
+   
+  };
+  useEffect(()=>{
+    dataApi()
+  },[])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.mainText}>FlatList with API</Text>
+      
+        {
+          data.length?
+          <FlatList
+          data={data}
+          renderItem={({item})=><View style={styles.main}>
+            <Text style={styles.ID}>  ID : {item.id}</Text>
+            <Text style={styles.title}>  TITLE : {item.title}</Text>
+            <Text style={styles.body}>    {item.body}</Text>
+          </View>}/>:null
+        }
+     
     </View>
   );
 }
@@ -13,8 +40,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mainText: {
+    fontSize: 30,
+    textAlign: "center",
+    backgroundColor: "#2F539B",
+    color: "white",
+    padding: 10,
+  },
+  ID:{
+    fontSize:20,
+    color:"white",
+    backgroundColor:"#2B3856",
+  },
+  title:{
+    fontSize:18,
+    color:"red",
+    backgroundColor:"#550A35"
+  },
+  body:{
+    fontSize:16,
+    color:"blue",
+    backgroundColor:"#A0D6B4"
+  },
+  main:{
+    borderBottomColor:"#0C090A",
+    borderBottomWidth:2,
+    padding:10,
+  }
 });
